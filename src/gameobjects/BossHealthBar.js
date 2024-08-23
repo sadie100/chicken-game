c3587119c1d2937c6d2cab72cee8d5a5e825df8b
sadie100 <03sadie@gmail.com>
2024-08-24 00:35:49 +0900

    헬스바 움직이는 오류 수정

--- src/gameobjects/BossHealthBar.js
+++ src/gameobjects/BossHealthBar.js
@@ -1,15 +1,16 @@
 export class BossHealthBar {
-    width = 300;
+    initialWidth = 300;
     constructor(scene) {
         this.scene = scene;
+        this.width = this.initialWidth;
         this.x = (this.scene.scale.width - this.width) / 2;
         this.y = 100;
 
-        this.fill = scene.add
-            .image(this.x, this.y, "health_bar_fill")
-            .setOrigin(0, 0.5);
         this.border = scene.add
             .image(this.x, this.y, "health_bar_border")
+            .setOrigin(0, 0.5);
+        this.fill = scene.add
+            .image(this.x, this.y, "health_bar_fill")
             .setOrigin(0, 0.5);
 
         // 이미지의 원래 비율 유지
@@ -19,6 +20,9 @@ export class BossHealthBar {
         this.border.setDisplaySize(this.width, this.height);
         this.fill.setDisplaySize(this.width, this.height);
 
+        // 초기 crop 설정
+        this.fill.setCrop(0, 0, this.width, this.height);
+
         this.setValue(1);
     }
 
@@ -26,8 +30,9 @@ export class BossHealthBar {
         if (percentage < 0) percentage = 0;
         if (percentage > 1) percentage = 1;
 
-        // fill 이미지의 너비를 직접 조절
-        this.fill.displayWidth = this.width * percentage;
+        // crop을 사용하여 fill 이미지의 보이는 부분을 조절
+        const cropWidth = 125 * percentage;
+        this.fill.setCrop(0, 0, cropWidth, this.height);
     }
 
     setPosition(x, y) {
