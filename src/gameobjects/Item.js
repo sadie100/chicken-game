231af066212b24ea0985cefe3028d10227746b32
Carpe-sehee <sadie100@carpecorp.com>
2025-09-24 21:46:21 +0900

    feat: 아이템 소멸 구현

--- src/gameobjects/Item.js
+++ src/gameobjects/Item.js
@@ -1,5 +1,5 @@
 import { Scene } from "phaser";
-import { ITEM_DEFS } from "../config/items";
+import { ITEM_DEFS, ITEM_TIMING_DEFAULT } from "../config/items";
 
 export class Item extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture, frame, id) {
@@ -26,6 +26,10 @@ export class Item extends Phaser.Physics.Arcade.Sprite {
         }
         // 아이템 설명 텍스트는 생성하지 않음 (요청에 따라 비활성화)
         this.descriptionText = null;
+
+        // 수명/깜빡임 설정 및 시작
+        const timing = (def && def.timing) || ITEM_TIMING_DEFAULT;
+        this.initLifetime(timing);
     }
 
     // 효과/제거/설명은 ITEM_DEFS에서 주입됨
@@ -43,6 +47,59 @@ export class Item extends Phaser.Physics.Arcade.Sprite {
 
     applyEffect(player) {
         this.effect(player);
+    }
+
+    initLifetime(timing) {
+        const { lifetimeMs, blinkStartMs, blinkIntervalMs } = timing;
+        this.blinkIntervalMs = blinkIntervalMs;
+
+        this.blinkTimer = this.scene.time.delayedCall(
+            blinkStartMs,
+            () => this.startBlink(),
+            null,
+            this
+        );
+
+        this.despawnTimer = this.scene.time.delayedCall(
+            lifetimeMs,
+            () => this.despawn(),
+            null,
+            this
+        );
+    }
+
+    startBlink() {
+        if (this.blinkTween) return;
+        this.blinkTween = this.scene.tweens.add({
+            targets: this,
+            alpha: 0.2,
+            duration: this.blinkIntervalMs,
+            yoyo: true,
+            repeat: -1,
+        });
+    }
+
+    stopBlink() {
+        if (this.blinkTween) {
+            this.scene.tweens.killTweensOf(this);
+            this.blinkTween = null;
+        }
+        this.setAlpha(1);
+    }
+
+    despawn() {
+        this.stopBlink();
+        this.body.enable = false;
+        this.setVisible(false);
+        this.destroy();
+    }
+
+    collect() {
+        if (this.blinkTimer) this.blinkTimer.remove();
+        if (this.despawnTimer) this.despawnTimer.remove();
+        this.stopBlink();
+        this.setVisible(false);
+        this.body.enable = false;
     }
 }
 
